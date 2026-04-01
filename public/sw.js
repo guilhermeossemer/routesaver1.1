92289656e516dbf7a9d1a8620842ab3420ad9b3c
Guilherme Ossemer <37717621+guilhermeossemer@users.noreply.github.com>
2026-04-01 16:35:06 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,25 @@
-const CACHE_NAME = "routesaver-v1";
+const CACHE_NAME = "routesaver-v2";
 
 const urlsToCache = [
-  "/routesaver-firebase/",
-  "/routesaver-firebase/index.html",
-  "/routesaver-firebase/public/manifest.webmanifest",
-  "/routesaver-firebase/public/css/auth.css",
-  "/routesaver-firebase/public/css/dashboard.css",
-  "/routesaver-firebase/public/css/style.css",
-  "/routesaver-firebase/public/js/firebase-config.js",
-  "/routesaver-firebase/public/js/auth.js",
-  "/routesaver-firebase/public/js/dashboard.js",
-  "/routesaver-firebase/public/pages/register.html",
-  "/routesaver-firebase/public/pages/dashboard.html",
-  "/routesaver-firebase/public/img/favicon-64.png",
-  "/routesaver-firebase/public/img/favicon-192.png",
-  "/routesaver-firebase/public/img/favicon-512.png"
+  "./",
+  "./index.html",
+  "./public/manifest.webmanifest",
+  "./public/pages/dashboard.html",
+  "./public/pages/register.html",
+  "./public/css/dashboard.css",
+  "./public/css/auth.css",
+  "./public/css/style.css",
+  "./public/js/dashboard.js",
+  "./public/js/auth.js",
+  "./public/js/firebase-config.js",
+  "./public/img/favicon-192.png",
+  "./public/img/favicon-512.png"
 ];
 
+// install
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -25,7 +27,28 @@ self.addEventListener("install", (event) => {
   );
 });
 
+// activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// fetch
 self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+
+  // NÃO cachear externos (Firebase/CDN)
+  if (requestUrl.origin !== location.origin) return;
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
