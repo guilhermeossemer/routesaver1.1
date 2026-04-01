2f05fb9754ede283f035044d0d7e2896b1993723
Guilherme Ossemer <37717621+guilhermeossemer@users.noreply.github.com>
2026-04-01 17:18:27 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,54 @@
-const CACHE_NAME = "routesaver-v2";
+const CACHE_NAME = "routesaver-v4";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./public/manifest.webmanifest",
-  "./public/pages/dashboard.html",
-  "./public/pages/register.html",
-  "./public/css/dashboard.css",
-  "./public/css/auth.css",
-  "./public/css/style.css",
-  "./public/js/dashboard.js",
-  "./public/js/auth.js",
-  "./public/js/firebase-config.js",
-  "./public/img/favicon-192.png",
-  "./public/img/favicon-512.png"
+    "./",
+    "./manifest.webmanifest",
+    "./pages/dashboard.html",
+    "./pages/register.html",
+    "./css/auth.css",
+    "./css/dashboard.css",
+    "./css/style.css",
+    "./js/auth.js",
+    "./js/dashboard.js",
+    "./js/firebase-config.js",
+    "./img/favicon-64.png",
+    "./img/favicon-192.png",
+    "./img/favicon-512.png"
 ];
 
-// install
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
         })
-      );
-    }).then(() => self.clients.claim())
-  );
+    );
 });
 
-// fetch
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
 self.addEventListener("fetch", (event) => {
-  const requestUrl = new URL(event.request.url);
+    const requestUrl = new URL(event.request.url);
 
-  // NÃO cachear externos (Firebase/CDN)
-  if (requestUrl.origin !== location.origin) return;
+    // não interceptar arquivos externos
+    if (requestUrl.origin !== location.origin) return;
 
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
